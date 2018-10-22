1bb35a062054f2e0dad5b7f16cc4ff69a46be5c9
koo9ithub <kk223704@naver.com>
2018-10-23 08:05:32 +0900

    7th

--- main.c
+++ main.c
@@ -5,12 +5,18 @@
 
 int main(int argc, char *argv[]) {
 	
-	char c;
+	float i1;
+	float i2;
+	float f;
 	
-	printf("Input a character: ");
-	scanf("%c", &c);
+	printf("분자를 입력하세요: ");
+	scanf("%f", &i1);
 	
-	printf("The next char is %c", c+1);
+	printf("분모를 입력하세요: ");
+	scanf("%f", &i2);
+	
+	f = i1/i2;
+	printf("나누기의 결과는? %.4f", f); 
 	
 	return 0;
 }
